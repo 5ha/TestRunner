4725332b6ff7b76857659114ff97dd61cbee64a6
Shawn Seabrook <shawn.seabrook@outlook.com>
2018-07-22 08:49:04 +0100

    Moved build processor into s separate task

--- SocketServer/SocketServerHandler.cs
+++ SocketServer/SocketServerHandler.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace SocketServer
 {
-    public class SocketServerHandler :  ChannelHandlerAdapter
+    public class SocketServerHandler : ChannelHandlerAdapter
     {
         public override void ChannelRead(IChannelHandlerContext context, object message)
         {
@@ -27,35 +27,18 @@ namespace SocketServer
                     context.WriteAndFlushAsync(byteBuffer);
                 };
 
-                try
+                string m = buffer.ToString(Encoding.UTF8);
+                Console.WriteLine(m);
+
+                BuildRunRequest request = Newtonsoft.Json.JsonConvert.DeserializeObject<BuildRunRequest>(m);
+
+                BuildRunner buildRunner = new BuildRunner(request, sender);
+
+                //https://github.com/Azure/DotNetty/issues/265
+                Task.Run(() =>
                 {
-                    //ContainerHelper helper = new ContainerHelper();
-                    //var res = helper.ListContainers(true).Result;
-
-                    var res = Task.Run<int>(() => { Task.Delay(10000).Wait(); return 1; }).Result;
-
-                    //WebClient client = new WebClient();
-
-                        //// Add a user agent header in case the 
-                        //// requested URI contains a query.
-
-                        //client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-
-                        //Stream data = client.OpenRead("http://www.google.com");
-                        //StreamReader reader = new StreamReader(data);
-                        //string s = reader.ReadToEnd();
-
-                        string m = buffer.ToString(Encoding.UTF8);
-                    Console.WriteLine(m);
-
-                    BuildRunRequest request = Newtonsoft.Json.JsonConvert.DeserializeObject<BuildRunRequest>(m);
-
-                    BuildRunner buildRunner = new BuildRunner(request, sender);
-                    buildRunner.StartBuild().Wait();
-                }catch(Exception e)
-                {
-                   var x = e.Message;
-                }
+                    buildRunner.StartBuild();
+                });
             }
         }
 
