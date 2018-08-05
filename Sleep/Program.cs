e8c7b1d6ae85c426ef861e6348a9ca7aa0d14a3c
Shawn Seabrook <shawn.seabrook@outlook.com>
2018-08-05 10:54:05 +0100

    Added sleeper

---
+++ Sleep/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            while (string.IsNullOrEmpty(s))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
