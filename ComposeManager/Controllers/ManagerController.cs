a223a4b64c68a6aa1624c1aaa1bae18364b81fed
Shawn Seabrook <shawn.seabrook@outlook.com>
2018-11-25 13:20:30 +0000

    Added ManagerController

---
+++ ComposeManager/Controllers/ManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComposeManager.Controllers
+{
+    public class ManagerController : ControllerBase
+    {
+        [HttpGet("/healthcheck")]
+        public ActionResult<string> Index()
+        {
+            return "SUCCESS";
+        }
+    }
+}
